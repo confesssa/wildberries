bc90f5539d5339ddf910d9a18a584a558ba98b13
volha <v.khviadchenia@gmail.com>
2022-02-23 23:43:36 +0100

    feat: add function for modal window

---
+++ js/cart.js
@@ -0,0 +1,11 @@
+const cartBnt = document.querySelector('.button-cart');
+const cart = document.getElementById('modal-cart');
+const closeBtn = cart.querySelector('.modal-close');
+
+cartBnt.addEventListener('click', function() {
+  cart.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', function() {
+  cart.style.display = '';
+})
