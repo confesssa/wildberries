5307795234f45c790eecec53477c69923589b571
volha <v.khviadchenia@gmail.com>
2022-03-01 05:22:00 +0100

    feat: implement filter for goods

--- js/get-goods.js
+++ js/get-goods.js
@@ -1,6 +1,32 @@
 const getGoods = () => {
 const links = document.querySelectorAll('.navigation-link');
 
+const renderGoods = (goods) => {
+  const goodsContainer = document.querySelector('.long-goods-list');
+
+  goodsContainer.innerHTML = '';
+  
+  goods.forEach((good) => {
+    const goodBlock = document.createElement('div');
+    goodBlock.classList.add('col-lg-3');
+    goodBlock.classList.add('col-sm-6');
+
+    goodBlock.innerHTML = `
+      <div class="goods-card">
+        <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+        <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+        <h3 class="goods-title">${good.name}</h3>
+        <p class="goods-description">${good.description}</p>
+        <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+          <span class="button-price">$${good.price}</span>
+        </button>
+      </div>
+    `;
+
+    goodsContainer.append(goodBlock);
+  })
+}
+
 const getData = (value, category) => {
   fetch('https://test-3fd0f-default-rtdb.firebaseio.com/db.json')
     .then((res) => {
@@ -8,8 +34,15 @@ const getData = (value, category) => {
     })
     .then((data) => {
       const array = category ? data.filter((item) => item[category] === value) : data;
+
       localStorage.setItem('goods', JSON.stringify(array));
-      console.log(JSON.parse(localStorage.getItem('goods')));
+      
+      console.log(window.location);
+      if (window.location.pathname !== '/goods.html') {
+        window.location.href = './goods.html';
+      } else {
+        renderGoods(array);
+      }
     })
 };
 
@@ -21,6 +54,10 @@ links.forEach((link) => {
     getData(linkValue, category);
   })
 })
+
+if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+  renderGoods(JSON.parse(localStorage.getItem('goods')))
+}
 };
 
 getGoods();
