01960a2c07757c8eb1002d8b8a56fbf0af69cae7
volha <v.khviadchenia@gmail.com>
2022-03-03 23:13:38 +0100

    feat: update function cart

--- js/cart.js
+++ js/cart.js
@@ -2,6 +2,30 @@ function cart() {
   const cartBnt = document.querySelector('.button-cart');
 const cart = document.getElementById('modal-cart');
 const closeBtn = cart.querySelector('.modal-close');
+const goodsContainer = document.querySelector('.long-goods-list');
+
+const addToCart = (id) => {
+  const goods = JSON.parse(localStorage.getItem('goods'));
+  const clickedGood = goods.find(good => good.id === id);
+
+  const cart = localStorage.getItem('cart') ?
+    JSON.parse(localStorage.getItem('cart')) :
+    [];
+
+    if (cart.some(good => good.id === clickedGood.id)) {
+      cart.map((good) => {
+        if (good.id === clickedGood.id) {
+          good.count++;
+        }
+        return good;
+      })
+    } else {
+      clickedGood.count = 1;
+      cart.push(clickedGood)
+    };
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 cartBnt.addEventListener('click', () => {
   cart.style.display = 'flex';
@@ -10,6 +34,17 @@ cartBnt.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   cart.style.display = '';
 });
+
+if (goodsContainer) {
+  goodsContainer.addEventListener('click', (event) => {
+    if (event.target.closest('.add-to-cart')) {
+      const buttonToCart = event.target.closest('.add-to-cart');
+      const goodId = buttonToCart.dataset.id;
+
+      addToCart(goodId);
+    }
+  })
+}
 };
 
 cart();
